de10a9d1e21a2dbcc0b70dc0c55d2d04cc96417d
PeaceSMC <macio-111@o2.pl>
2016-11-25 16:18:48 +0100

    Huge progress

--- interpreter.py
+++ interpreter.py
@@ -8,14 +8,17 @@ bot.unicode_punctuation = re.compile(r'[.,!?;:]')
 bot.load_directory("./resources")
 bot.sort_replies()
 
-print("Witaj! Mam na imię CorpoBot i moim zadaniem jest przybliżyć Ci ofertę firmy Corpo. Możemy też trochę pogadać.\n" \
-      "Jeśli Ci się znudzi, po prostu wpisz 'Koniec'!\n"
+print("Witaj! Mam na imię CorpoBot i moim zadaniem jest przybliżyć Ci ofertę firmy Corpo. Możemy też trochę pogadać.\n"
+      "Jeśli Ci się znudzi, po prostu wpisz 'end'!\n"
+      "Jeśli brakuje Ci pomysłu, o czym moglibyśmy pogadać, poproś mnie o pomoc.\n"
       "W czym mogę Ci pomóc?")
+
 while True:
 
-    msg = polishDel(input('Ty > '))\
-#   print("napisałeś '" + msg + "'")
-    if msg == 'Koniec':
+    msg = polishDel(input('Ty > '))
+    msg = re.sub(r':[\)*\(]', "", msg)
+    #print("napisałeś '" + msg + "'")
+    if msg == 'end':
         quit()
 
     reply = bot.reply("localuser", msg)
